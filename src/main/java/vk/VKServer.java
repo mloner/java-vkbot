3a01c903f98760bf652d71a2426b199ff1e2ae69
mloner <shipshon@gmail.com>
2020-04-30 10:26:04 +0400

    qwe

--- src/main/java/vk/VKServer.java
+++ src/main/java/vk/VKServer.java
@@ -36,8 +36,6 @@ public class VKServer {
                 final int RECONNECT_TIME = 10000;
                 System.out.println("Повторное соединение через " + RECONNECT_TIME / 1000 + " секунд");
                 Thread.sleep(RECONNECT_TIME);
-            } catch(ApiException e) {
-            	System.out.println("Blyad. Api exception");
             }
         }
     }
